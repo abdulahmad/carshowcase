ed326511d63f870b352ecf337da89db67b4b7847
Abdul Ahmad <abdul.ahmad@gmail.com>
2018-02-25 18:45:13 -0500

    more eslint changes

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -5,19 +5,19 @@ module.exports = {
   entry: './src/client/index.js',
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, '../dist')
+    path: path.resolve(__dirname, '../dist'),
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader'
-      }
-    ]
+        loader: 'babel-loader',
+      },
+    ],
   },
   devServer: {
     index: '../src/index.html',
     open: true,
-    publicPath: "/"
-  }
+    publicPath: '/',
+  },
 };
--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import HtmlHead from './components/HtmlHead/HtmlHead';
 // import Page from './components/Page/Page';
 
-class App extends Component {
+class App extends Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className="App">
