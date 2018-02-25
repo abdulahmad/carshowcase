3ab1687860e2a8763e2713f92b72a9b9bda301f8
Abdul Ahmad <abdul.ahmad@gmail.com>
2018-02-25 01:22:32 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+// import HtmlHead from './components/HtmlHead/HtmlHead';
+// import Page from './components/Page/Page';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        { /* <HtmlHead
+          title={data.Title}
+          metaDescription={data.Excerpt}
+          metaImage={data.MainImageUrl}
+          metaUrl=""
+        />
+        <Page data={data} /> */ }
+      </div>
+    );
+  }
+}
+
+export default App;
