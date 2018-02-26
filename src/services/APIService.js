56225ace2f083c8ae39ff5a6a4ca3fc9e5df6906
Abdul Ahmad <abdul.ahmad@gmail.com>
2018-02-25 22:05:50 -0500

    Homepage

---
+++ src/services/APIService.js
@@ -0,0 +1,10 @@
+import AvailabilityResponseInStock from './mock/availabilityResponseInStock.json';
+import AvailabilityResponseOutOfStock from './mock/availabilityResponseOutOfStock.json';
+import AvailabilityResponseUnavailable from './mock/availabilityResponseUnavailable.json';
+import CarsResponse from './mock/CarsResponse.json';
+
+class APIService {
+
+}
+
+export default APIService;
