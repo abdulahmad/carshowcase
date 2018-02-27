fb95ecb38d325d769af05668f09d7eb25b955eea
Abdul Ahmad <abdul.ahmad@gmail.com>
2018-02-26 22:28:35 -0500

    cartile

--- src/components/homepage/CarTile/CarTile.jsx
+++ src/components/homepage/CarTile/CarTile.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import Button from '../Button/Button.jsx';
 import './CarTile.scss';
 
-const CarTile = () => (
-  <div className="CarTile" />
+function getBuyButton(availability) {
+  if (availability === 'In Dealership') {
+    return <Button />;
+  }
+  return '';
+}
+
+const CarTile = ({
+  image, name, make, model, year, availability,
+}) => (
+  <div className="CarTile">
+    <img className="CarTile__img" src={image} alt={name} />
+    <span className="CarTitle__name">{ name }</span>
+    <span className="CarTitle__availability">{availability}</span>
+    {getBuyButton(availability)}
+  </div>
 );
 
 export default CarTile;
 
 CarTile.propTypes = {
   // authorBio: PropTypes.object.isRequired,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  make: PropTypes.string.isRequired,
+  model: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  availability: PropTypes.string.isRequired,
 };
--- src/services/APIService.js
+++ src/services/APIService.js
@@ -1,10 +1,26 @@
 import AvailabilityResponseInStock from './mock/availabilityResponseInStock.json';
 import AvailabilityResponseOutOfStock from './mock/availabilityResponseOutOfStock.json';
 import AvailabilityResponseUnavailable from './mock/availabilityResponseUnavailable.json';
-import CarsResponse from './mock/CarsResponse.json';
+import CarsResponse from './mock/carsResponse.json';
 
+/**
+ * This really is a mock API Service. If there was a real API integration, I'd
+ * seperate it out so that you had an API Factory which would return you a
+ * real API Service or mock API Service depending on the environment settings.
+ */
 class APIService {
+  static getAvailability(id) {
+    if (id === 1) {
+      return AvailabilityResponseOutOfStock;
+    } else if (id === 2) {
+      return AvailabilityResponseUnavailable;
+    }
+    return AvailabilityResponseInStock;
+  }
 
+  static getCars() {
+    return CarsResponse;
+  }
 }
 
 export default APIService;
