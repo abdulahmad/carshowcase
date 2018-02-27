f36eba527bca88ba10bb67b399852b0655109bdf
Abdul Ahmad <abdul.ahmad@gmail.com>
2018-02-27 01:57:56 -0500

    add scss support

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -13,6 +13,16 @@ module.exports = {
   module: {
     rules: [
       // add your custom rules.
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader', // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader', // compiles Sass to CSS
+        }],
+      },
     ],
   },
 };
